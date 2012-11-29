fc176e94268780898a49d8166a6677060e314601
Erick Rivas <chemical.rivas@gmail.com>
2012-11-28 19:28:07 -0500

    Added blacklist, addt'l file support, moved constants from file

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
-var net = require("net");
-var stream = require("stream");
-var fs = require("fs");
+var net = require("net"), stream = require("stream"), fs = require("fs");
+var path = require("path"), curpath = path.dirname(), urllib = require("url");
 var port = 25000;
-var curpath = require('path').dirname();
+require("./defs");
+blacklist = new Array("server.js", "defs.js");
+
 httpserver = net.createServer(function(listener){
-  // http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.1
-  STATUS_PHRASE = {
-    "100":  "Continue", "101":  "Switching Protocols",
-    "200":  "OK", "201":  "Created", "202":  "Accepted", "203":  "Non-Authoritative Information",
-      "204":  "No Content", "205":  "Reset Content", "206":  "Partial Content",
-    "300":  "Multiple Choices", "301":  "Moved Permanently", "302":  "Found", "303":  "See Other",
-      "304":  "Not Modified", "305":  "Use Proxy", "307":  "Temporary Redirect",
-    "400":  "Bad Request", "401":  "Unauthorized", "402":  "Payment Required", "403":  "Forbidden",
-      "404":  "Not Found", "405":  "Method Not Allowed", "406":  "Not Acceptable",
-      "407":  "Proxy Authentication Required", "408":  "Request Time-out", "409":  "Conflict",
-      "410":  "Gone", "411":  "Length Required", "412":  "Precondition Failed", "413":  "Request Entity Too Large",
-      "414":  "Request-URI Too Large", "415":  "Unsupported Media Type", "416":  "Request range not satisfiable",
-      "417":  "Expectation Failed",
-    "500":  "Internal Server Error", "501":  "Not Implemented", "502":  "Bad Gateway", "503":  "Service Unavailable",
-      "504":  "Gateway Time-out", "505":  "HTTP Version not supported"
-  }
   listener.on('connect', function(){
     console.log("Incoming connection from " + listener.remoteAddress);
   });
   listener.on('data', function(data){
     var headers = data.toString().split("\r\n");
     var request = headers[0].split(" ");
-    var obj = "";
     var requestType = request[0], pageName = request[1], protocol = request[2];
     var reply = new Response();
-    if (requestType == "GET" || requestType == "POST")
+    console.log("Request Headers: " + data);
+    if ((requestType == "GET" || requestType == "POST") && !blacklist.contains(path.basename(pageName)))
     {
       if (endsWith("/", pageName))
       {
@@ -39,12 +24,16 @@ httpserver = net.createServer(function(listener){
       fs.exists(pageName, function(exists)
       {
         reply.header += getResponseStatus(exists?200:404);
-        pageName = exists?pageName:curpath + "/404.html";
-        reply.header += "Server: localhost:" + port + "\r\n";
+        pageName = exists?pageName:path.join(curpath, "404.html");
+//        reply.header += "Server: localhost:" + port + "\r\n";
+        // TODO: Is encoding OK being hard-coded or should it depend
+        // on filetype?
         fs.readFile(pageName, "UTF-8", function(err, data)
         {
-          console.log("Requested: " + pageName);
-          reply.header += "Content-length: " + data==undefined?0:data.length*2;
+          reply.header += "Content-length: ";
+          // TODO: Content-length depends on data type
+          reply.header += data==undefined?0:data.length*2 + "\r\n";
+          reply.header += "Content-type: " + getContentType(pageName.split(".").pop())
           reply.content = data;
           listener.write(reply.stringify());
           listener.end();
@@ -60,12 +49,19 @@ httpserver = net.createServer(function(listener){
 });
 
 function getResponseStatus(code){
-  console.log("Request code: " + code);
   return " " + code.toString() + " " + STATUS_PHRASE[code] + "\r\n";
 }
 
 function endsWith(needle, haystack){
   return haystack.slice(haystack.length - needle.length, needle.length) == needle;
+}
+
+Array.prototype.contains = function(value){
+  for (var i = 0; i < this.length; i++){
+    if (this[i] == value)
+      return true;
+  }
+  return false;
 }
 
 Response = function(){
@@ -74,7 +70,7 @@ Response = function(){
 };
 
 Response.prototype.stringify = function(){
-  console.log("Header: " + this.header);
+  console.log("Reply Header: " + this.header);
   console.log("Content: " + this.content);
   return this.header + "\r\n\r\n" + this.content;
 }
